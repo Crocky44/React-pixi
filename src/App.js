39f19a19c163b5ef9f0af943e27d0754b2eda230
Crocky44 <95232865+Crocky44@users.noreply.github.com>
2023-01-05 13:26:29 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
 import './App.css';
+import {useReducer, useTick, useRef} from 'react';
+import {Sprite, Stage, Container} from '@pixi/react-pixi';
+import * as PIXI from 'pixi.js';
 
-function App() {
+const App = () => {
+  return(
+    <div className="App">
+  <Stage width={300} height={300} options={{ backgroundColor: 0xeef1f5 }}>
+  <Container position={[150, 150]}>
+    <Sprite
+      anchor={0.5}
+      x={-75}
+      y={-75}
+      image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+    />
+    <Sprite
+      anchor={0.5}
+      x={0}
+      y={0}
+      image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+    />
+    <Sprite
+      anchor={0.5}
+      x={75}
+      y={75}
+      image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
+    />
+  </Container>
+</Stage>
+</div>)};
+
+const Bibi = () => {
+
+  return (
+  <p className="small">
+
+    Bonjour
+  </p>)
+};
+
+
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -25,5 +66,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
